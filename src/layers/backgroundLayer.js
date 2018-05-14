0a5b77aed39eaae53017b8b0c4d199e61a2c8049
Ryan Chen <xryanchen@gmail.com>
2018-05-14 01:04:10 -0700

    Refactor x and y axis calculation

--- src/layers/backgroundLayer.js
+++ src/layers/backgroundLayer.js
@@ -13,65 +13,10 @@ const backgroundLayer = {
   computeFinalRenderState: (store) => {
     console.log('backgroundlayer computeFinalRenderState', store)
 
-    // // X axis
-    // let xAxis = {
-    //   position: new Position({
-    //     top: store.yAxis.height,
-    //     left: store.yAxis.width,
-    //     width: store.xAxis.width,
-    //     height: store.xAxis.height
-    //   })
-    // }
-
-    // // Compute x axis header cell (in tree structure)
-    // xAxis.rootCell = new AxisCell({
-    //   parentPosition: xAxis.position,
-    //   locationType: 
-    // })
-
-    // Compute x axis header
-    // xAxis.axisHeader = new AxisHeader({
-    //   axisLevels: store.xAxis.axisLevels,
-    //   position: xAxis.position,
-    //   aspectType: aspectTypeE.X,
-    //   locationType: locationTypeE.BOTTOM
-    // })
-
-    // Y axis
-    let yAxis = {
-      position: new Position({
-        top: 0,
-        left: 0,
-        width: store.yAxis.width,
-        height: store.yAxis.height,
-      })
-    }
-
-    // // Compute y axis header
-    // yAxis.axisHeader = new AxisHeader({
-    //   axisLevels: store.yAxis.axisLevels,
-    //   position: yAxis.position,
-    //   aspectType: aspectTypeE.Y,
-    //   locationType: locationTypeE.LEFT
-    // })
-
-    // X axis cells
     // Here we need to do three things:
-    // 1. compute the x axis cell in tree nodes.
+    // 1. compute the axis cell in tree nodes.
     // 2. compose map for nodeId -> node
     // 3. compose map for markId -> nodeId
-    // xAxis.axisHeader = new Tree(store.xAxis.axisLevels)
-    // xAxis.rootCell = 
-
-    // xAxis.cells = store.xAxis.valueOrder.map((value, valueIndex) => {
-    //   const tickCount = store.xAxis.valueOrder,
-    //         tickInterval = xAxis.position.width / (tickCount + 1),
-    //         position = {
-    //           top: xAxis.position.top,
-    //           left: xAxis.position.left + (valueIndex + 1) * tickInterval,
-    //           width: tickInterval
-    //         }
-    // })
 
     const axisOffset = backgroundLayer._getAxisOffset(store)
 
@@ -89,10 +34,10 @@ const backgroundLayer = {
       // chartHeight: store.chart.height,
 
       // X axis
-      xAxis: backgroundLayer._getXAxisRenderState(store, axisOffset),
+      xAxis: backgroundLayer._getAxisRenderState(aspectTypeE.X, store, axisOffset),
 
       // Y axis position
-      yAxis,
+      yAxis: backgroundLayer._getAxisRenderState(aspectTypeE.Y, store, axisOffset),
       
       // Scene position
       // sceneTop: 0,
@@ -137,100 +82,89 @@ const backgroundLayer = {
     return axisOffset
   },
 
-  /**
-   * Get the render state for x axis
-   */
-  _getXAxisRenderState: (store, axisOffset) => {
-    if (store.xAxis.isHidden) {
+  _getAxisRenderState: (aspectType, store, axisOffset) => {
+    let axisState,
+        mainAxisSize
+
+    switch (aspectType) {
+      case aspectTypeE.X:
+        axisState = store.xAxis
+        mainAxisSize = store.xAxis.width
+        break
+
+      case aspectTypeE.Y:
+        axisState = store.yAxis
+        mainAxisSize = store.yAxis.height
+        break
+      
+      default:
+        throw new Error(`Unexpected aspect type: ${aspectType}`)
+    }
+    
+    if (axisState.isHidden) {
       // When x axis is hidden, the render state is null
       return null
     }
 
-    let xAxisRenderState = {}
+    let axisRenderState = {}
 
-    // X axis can be top or bottom depend on the axis states
-    xAxisRenderState[locationTypeE.TOP] = {
+    // Compute all locations for axis
+    axisRenderState[locationTypeE.TOP] = {
       position: new Position({
         top: axisOffset[locationTypeE.TOP],
         left: axisOffset[locationTypeE.LEFT],
-        width: store.xAxis.width,
-        height: store.xAxis.height
+        width: axisState.width,
+        height: axisState.height
       }),
       // The axis cell map on axis
       axisCellMapByLevel: {}
     }
 
-    xAxisRenderState[locationTypeE.BOTTOM] = {
+    axisRenderState[locationTypeE.BOTTOM] = {
       position: new Position({
         top: store.chart.height - axisOffset[locationTypeE.BOTTOM],
         left: axisOffset[locationTypeE.LEFT],
-        width: store.xAxis.width,
-        height: store.xAxis.height
+        width: axisState.width,
+        height: axisState.height
+      }),
+      // The axis cell map on axis
+      axisCellMapByLevel: {}
+    }
+
+    axisRenderState[locationTypeE.LEFT] = {
+      position: new Position({
+        top: axisOffset[locationTypeE.TOP],
+        left: axisOffset[locationTypeE.LEFT],
+        width: axisState.width,
+        height: axisState.height
+      }),
+      // The axis cell map on axis
+      axisCellMapByLevel: {}
+    }
+
+    axisRenderState[locationTypeE.RIGHT] = {
+      position: new Position({
+        top: axisOffset[locationTypeE.TOP],
+        left: store.chart.width - axisOffset[locationTypeE.RIGHT],
+        width: axisState.width,
+        height: axisState.height
       }),
       // The axis cell map on axis
       axisCellMapByLevel: {}
     }
 
     // All used for calculating position information for axis cells
-    xAxisRenderState.mainAxisSize = store.xAxis.width
-    xAxisRenderState.markSize = Math.max(theme.axis.mark.minSize, store.xAxis.width / store.xAxis.axisLevels[0].values.length)
+    axisRenderState.mainAxisSize = mainAxisSize
+    axisRenderState.markSize = Math.max(theme.axis.mark.minSize, mainAxisSize / axisState.axisLevels[0].values.length)
 
     // Compute x axis header cell (in tree structure)
-    xAxisRenderState.rootAxisCell = AxisCell.buildTree({
+    axisRenderState.rootAxisCell = AxisCell.buildTree({
       // level states will be used by reference. Should not be mutated.
-      levelStates: store.xAxis.axisLevels,
-      axisRenderState: xAxisRenderState
+      levelStates: axisState.axisLevels,
+      axisRenderState
     })
 
-    return xAxisRenderState
-  },
-
-  /**
-   * Get the render state for y axis
-   */
-  _getYAxisRenderState: (store) => {
-    if (store.yAxis.isHidden) {
-      // When x axis is hidden, the render state is null
-      return null
-    }
-
-    let yAxisRenderState = {}
-
-    // X axis can be top or bottom depend on the axis states
-    yAxisRenderState[locationTypeE.LEFT] = {
-      position: new Position({
-        top: store.xAxis.height,
-        left: store.yAxis.width,
-        width: store.xAxis.width,
-        height: store.xAxis.height
-      }),
-      // The axis cell map on axis
-      axisCellMapByLevel: {}
-    }
-
-    yAxisRenderState[locationTypeE.BOTTOM] = {
-      position: new Position({
-        top: store.yAxis.height,
-        left: store.yAxis.width,
-        width: store.xAxis.width,
-        height: store.xAxis.height
-      }),
-      // The axis cell map on axis
-      axisCellMapByLevel: {}
-    }
-
-    // All used for calculating position information for axis cells
-    yAxisRenderState.mainAxisSize = store.xAxis.width
-    yAxisRenderState.markSize = Math.max(theme.axis.mark.minSize, store.xAxis.width / store.xAxis.axisLevels[0].values.length)
-
-    // Compute x axis header cell (in tree structure)
-    yAxisRenderState.rootAxisCell = AxisCell.buildTree({
-      // level states will be used by reference. Should not be mutated.
-      levelStates: store.xAxis.axisLevels,
-      axisRenderState: yAxisRenderState
-    })
-
-    return yAxisRenderState
+    return axisRenderState
   }
 }
 
